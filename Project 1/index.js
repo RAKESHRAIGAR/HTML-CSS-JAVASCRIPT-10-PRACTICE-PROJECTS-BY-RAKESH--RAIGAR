4a0a46991de6229736cfc4e016fa6f5a421734d2
RAKESH RAIGAR <rkshsukhen@gmail.com>
2023-01-30 18:04:25 +0530

    added innerhtml

--- Project 1/index.js
+++ Project 1/index.js
@@ -26,9 +26,26 @@ const toggleDateOfBirthSelector = () => {
     isDOBOpen = !isDOBOpen;
     console.log("Toggle", isDOBOpen);
 };
+const updateAge = () => {
+    const currentDate = new Date();
+    const dateDiff = currentDate - dateOfBirth;
+    const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
+    const month = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365) % 12);
+    const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
+    const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
+    const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
+    const second = Math.floor(dateDiff / 1000) % 60;
+    yearEl.innerHTML = year;
+    monthEl.innerHTML = month;
+    dayEl.innerHTML = day;
+    hourEl.innerHTML = hour;
+    minuteEl.innerHTML = minute;
+    secondEl.innerHTML = second;
+ };
 
 const setDOBHandler = () => {
-dateOfBirth = dobInputEl.value;
+const dateString = dobInputEl.value;
+dateOfBirth = new Date(dateString);
 if(dateOfBirth){
     initialTextEl.classList.add("hide");
     afterDOBBtnTextEl.classList.remove("hide");
@@ -41,17 +58,7 @@ if(dateOfBirth){
 };
 
  setDOBHandler();
- const updateAge = () => {
-    const currentDate = new Date();
-    const dateDiff = currentDate - dateOfBirth;
-    const year = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365));
-    const month = Math.floor(dateDiff / (1000 * 60 * 60 * 24 * 365) % 12);
-    const day = Math.floor(dateDiff / (1000 * 60 * 60 * 24)) % 30;
-    const hour = Math.floor(dateDiff / (1000 * 60 * 60)) % 24;
-    const minute = Math.floor(dateDiff / (1000 * 60)) % 60;
-    const second = Math.floor(dateDiff / 1000) % 60;
-    yearEl.innerHTML = year;
- }
+
 
 settingCogEl.addEventListener("click", toggleDateOfBirthSelector);
 dobButtonEl.addEventListener("click", setDOBHandler);
