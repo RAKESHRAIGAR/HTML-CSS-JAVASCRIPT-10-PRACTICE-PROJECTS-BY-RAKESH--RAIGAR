096e971a76a419208cc69778191738529940a18d
RAKESH RAIGAR <rkshsukhen@gmail.com>
2023-02-03 10:54:56 +0530

    project3 added

---
+++ Project 3 Calculate-marks/index.js
@@ -0,0 +1,16 @@
+const calculateFormEl = document.getElementById("calculateForm"); 
+const resultEl = document.getElementById("result"); 
+const calculateMarks = (event) => {
+const maxMarks = 400;   
+ event.preventDefault();  
+ const formdata = new FormData(calculateFormEl);
+ const data = {};
+ formData.forEach((value, key) => {
+    data[key] = +value;
+ });  
+ const totalMarks = data.maths + data.english + data.hindi + data.science;
+ const percentage = Math.round(totalMarks/maxMarks) * 100;
+ 
+ resultEl.innerText = `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
+
+};
