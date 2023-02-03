057a37cd740e3386205223a52925f9b69c59fc53
RAKESH RAIGAR <rkshsukhen@gmail.com>
2023-02-03 11:16:02 +0530

    html correction

--- Project 3 Calculate-marks/index.js
+++ Project 3 Calculate-marks/index.js
@@ -3,13 +3,13 @@ const resultEl = document.getElementById("result");
 const calculateMarks = (event) => {
 const maxMarks = 400;   
  event.preventDefault();  
- const formdata = new FormData(calculateFormEl);
+ const formData = new FormData(calculateFormEl);
  const data = {};
  formData.forEach((value, key) => {
     data[key] = +value;
  });  
- const totalMarks = data.maths + data.english + data.hindi + data.science;
- const percentage = Math.round(totalMarks/maxMarks) * 100;
+ const totalMarks = data.maths + data.science + data.hindi + data.english;
+ const percentage = Math.round((totalMarks/maxMarks) * 100);
  
  resultEl.innerText = `You have got ${totalMarks} marks out of ${maxMarks} and your percentage is ${percentage}%`;
 
